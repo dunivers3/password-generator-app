ed832a10495cf79cf860e224018e69ba95c10c45
Duncan <wangari.dun@gmail.com>
2022-02-12 09:58:00 +0300

    Correction for js randomization

--- index.js
+++ index.js
@@ -1,83 +1,39 @@
 //Generate random capital letters
-const upperCaseLetters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
-let caps = upperCaseLetters[randomCapitalLetters()];
-//console.log(caps);
+const passwordChars = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", 1, 2, 3, 4, 5, 6, 7, 8, 9, 0, ",", ".", "~", "!",";", ":", "?", "/", "&", "^", "#", "=", "$", "+", "*", "%", "'", "|", "'", "<", ">", "@"]
 
-//Generate random small letters
-const lowerCaseLetters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
-let small = lowerCaseLetters[randomSmallLetters()];
-//console.log(small);
-
-//Generate random numbers
-const numbersLength = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0];
-let num = numbersLength[randomNumbers()];
-//console.log(num);
-
-//Generate random special characters
-const otherChars = [",", ".", "~", "!",";", ":", "?", "/", "&", "^", "#", "=", "$", "+", "*", "%", "'", "|", "'", "<", ">", "@"];
-let chars = otherChars[randomCharacters()];
-//console.log(chars);
+//let randomChar = Math.floor(Math.random() * passwordChars.length);
+//let passCode = passwordChars[randomChar];
 
 
-let combinedFunctions = [
-    num,
-    chars,
-    caps,
-    small
-]
+
+
+
+
+
 
 //Render out the generated passwords in the output holders
-let rectangleEl = document.getElementById("rectangle_el");
+let boxEl1 = document.getElementById("box_1");
+let boxEl2 = document.getElementById("box_2");
+let boxEl3 = document.getElementById("box_3");
+let boxEl4 = document.getElementById("box_4");
 
 
 //GENERATE PASSWORD (CLICKED!)
-function genPass(){
-    rectangleEl.textContent += combinedFunctions[randomPassword()];
-}
-
-
-
-//CAPITAL LETTERS
-function randomCapitalLetters(){
-    return Math.floor(Math.random() * upperCaseLetters.length);
-}
-
-//SMALL LETTERS
-function randomSmallLetters(){
-    return Math.floor(Math.random() * lowerCaseLetters.length);
-}
-
-//NUMBERS
-function randomNumbers(){
-    return Math.floor(Math.random()* numbersLength.length);
-}
-//SPECIAL CHARACTERS
-function randomCharacters(){
-    return Math.floor(Math.random()* otherChars.length);
-}
-
-//COMBINED FUNCTION ARRAY
-function randomPassword(){
-    return Math.floor(Math.random()* combinedFunctions.length);
-}
-
-
-console.log(randomPassword())
-
-
-
-
-//return 
-
-
-
-
-/*
-function smallLetter(){
-    for(i = 0; i != upperCaseLetters.length; i++){
-        let smallLetter =  upperCaseLetters[i].toLowerCase();
-        return smallLetter;
+function generate(){
+    for(i = 0; i < 15; i ++){
+        boxEl1.textContent += passwordChars[randomFunctionGenerator()];
+        boxEl2.textContent += passwordChars[randomFunctionGenerator()];
+        boxEl3.textContent += passwordChars[randomFunctionGenerator()];
+        boxEl4.textContent += passwordChars[randomFunctionGenerator()];
     }
 }
 
- */
+//This function creates a random passwordChars index for each value in the array
+function randomFunctionGenerator(){
+    return Math.floor(Math.random() * passwordChars.length);
+}
+
+
+function boxSelector(){
+
+}
