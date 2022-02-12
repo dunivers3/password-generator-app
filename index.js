a216aced18c739491ea55b14bda38807101764dc
Duncan <wangari.dun@gmail.com>
2022-02-12 12:10:37 +0300

    Enables random passcode on all empty boxes

--- index.js
+++ index.js
@@ -1,15 +1,6 @@
 //Generate random capital letters
 const passwordChars = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", 1, 2, 3, 4, 5, 6, 7, 8, 9, 0, ",", ".", "~", "!",";", ":", "?", "/", "&", "^", "#", "=", "$", "+", "*", "%", "'", "|", "'", "<", ">", "@"]
 
-//let randomChar = Math.floor(Math.random() * passwordChars.length);
-//let passCode = passwordChars[randomChar];
-
-
-
-
-
-
-
 
 //Render out the generated passwords in the output holders
 let boxEl1 = document.getElementById("box_1");
@@ -18,22 +9,35 @@ let boxEl3 = document.getElementById("box_3");
 let boxEl4 = document.getElementById("box_4");
 
 
+
 //GENERATE PASSWORD (CLICKED!)
 function generate(){
     for(i = 0; i < 15; i ++){
-        boxEl1.textContent += passwordChars[randomFunctionGenerator()];
-        boxEl2.textContent += passwordChars[randomFunctionGenerator()];
-        boxEl3.textContent += passwordChars[randomFunctionGenerator()];
-        boxEl4.textContent += passwordChars[randomFunctionGenerator()];
+        boxEl1.textContent += "";
+        boxEl1.textContent += passwordChars[randomCharGenerator()];
+        boxEl1.style.color = "#55F991";
+        boxEl2.textContent += passwordChars[randomCharGenerator()];
+        boxEl2.style.color = "#55F991";
+        boxEl3.textContent += passwordChars[randomCharGenerator()];
+        boxEl3.style.color = "#55F991";
+        boxEl4.textContent += passwordChars[randomCharGenerator()];
+        boxEl4.style.color = "#55F991";
     }
 }
 
 //This function creates a random passwordChars index for each value in the array
-function randomFunctionGenerator(){
+function randomCharGenerator(){
     return Math.floor(Math.random() * passwordChars.length);
 }
 
 
-function boxSelector(){
 
-}
+//Whats left
+
+// Generating the a random/ user preference password length
+
+// Have the app reset on each "Generate password " button press
+
+// Make passcode easy to save to clipboard on a single click
+
+// Able to populate different passcode lengths on each box
